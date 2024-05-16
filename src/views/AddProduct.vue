5bf0797ea64af9011839832c83618de6d20441f0
dipaanggra <gadaakuunn@gmail.com>
2024-05-16 11:51:28 +0700

    first commit

--- src/views/AddProduct.vue
+++ src/views/AddProduct.vue
@@ -48,23 +48,32 @@ export default {
                 </svg>
             </button>
             <h2 class="text-2xl font-bold mb-4">Add Product</h2>
-            <div class="flex flex-col gap-4">
+            <div class="flex flex-col gap-3 justify-between">
                 <label
-                    class="flex flex-col items-center justify-center border-2 border-dashed rounded-lg h-48 w-64 cursor-pointer bg-blue-50">
+                    class="flex flex-col items-center justify-center border-2 border-dashed rounded-lg h-36 w-40 cursor-pointer bg-blue-50">
                     <input type="file" class="hidden" @change="handleImageUpload" />
                     <span class="text-blue-500">Upload Image</span>
                 </label>
-                <input type="text" v-model="productName" placeholder="Product Name" class="border rounded px-4 py-2" />
-                <input type="text" v-model="price" placeholder="Input price" class="border rounded px-4 py-2" />
-                <select v-model="category" class="border rounded px-4 py-2">
-                    <option value="" disabled>Select category</option>
-                    <option value="Makanan">Makanan</option>
-                    <option value="Minuman">Minuman</option>
-                    <option value="Lainnya">Lainnya</option>
-                </select>
+                <div class="absolute right-0 z-10 w-60 mb-6 rounded-md mr-3">
+                    <lable class="mt-5 px-2 text-sm">Product Name</lable>
+                    <input type="text" v-model="productName" placeholder="Product Name"
+                        class="border rounded px-4 py-2 border-blue-500" />
+                    <label class="px-2 text-sm">Price</label>
+                    <input type="text" v-model="price" placeholder="Input price"
+                        class="border rounded px-4 py-2 border-blue-500" />
+                    <!-- <label class="px-2 text-sm">Category</label>
+                    <select v-model="category" class="border rounded px-4 py-2 border-blue-500">
+                        <option value="" disabled>Select category</option>
+                        <option value="Makanan">Makanan</option>
+                        <option value="Minuman">Minuman</option>
+                        <option value="Lainnya">Lainnya</option>
+                    </select> -->
+                </div>
                 <div class="flex justify-end gap-4 mt-4">
-                    <button @click="$emit('close')" class="rounded bg-gray-200 px-4 py-2">Cancel</button>
-                    <button @click="addProduct" class="rounded bg-blue-500 px-4 py-2 text-white">Confirm</button>
+                    <button @click="$emit('close')"
+                        class="rounded border-2 border-blue-500 hover:border-blue-600 px-4 py-2 text-blue-500 hover:text-blue-600">Cancel</button>
+                    <button @click="addProduct"
+                        class="rounded bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white">Confirm</button>
                 </div>
             </div>
         </div>
